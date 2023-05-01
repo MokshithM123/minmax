52800dfd2c0be26566d1aa20ec58532984b30b32
labs34 <131676255+labs34@users.noreply.github.com>
2023-05-01 20:58:27 +0530

    Update minmax.scala

--- minmax.scala
+++ minmax.scala
@@ -1,45 +1,29 @@
-object ImperativeQuickSort {
-  def sort(a: Array[Int]) {
+import scala.io.StdIn
+import scala.collection.mutable.ArrayBuffer
 
-    def swap(i: Int, j: Int) {
-      val t = a(i); a(i) = a(j); a(j) = t
-    }
-
-    def sort1(l: Int, r: Int) {
-      val pivot = a((l + r) / 2)
-      var i = l
-      var j = r
-      while (i <= j) {
-        while (a(i) < pivot) i += 1
-        while (a(j) > pivot) j -= 1
-        if (i <= j) {
-          swap(i, j)
-          i += 1
-          j -= 1
-        }
-      }
-      if (l < j) sort1(l, j)
-      if (j < r) sort1(i, r)
-    }
-
-    if (a.length > 0)
-      sort1(0, a.length - 1)
-  }
-
-  def println(ar: Array[Int]) {
-    def print1 = {
-      def iter(i: Int): String =
-        ar(i) + (if (i < ar.length-1) "," + iter(i+1) else "")
-      if (ar.length == 0) "" else iter(0)
-    }
-    Console.println("[" + print1 + "]")
-  }
-
-  def main(args: Array[String]) {
-    var ar = Array(6,5,2,1,8);
-    println(ar)
-    sort(ar)
-    println(ar)
-  }
-
-}
+object MinMax{
+	def main(args: Array[String]):Unit = {
+		var numArray = new ArrayBuffer[Int]()
+		println("Enter no. of elements")
+		val n = scala.io.StdIn.readInt()
+		println("Enter elements")
+		for(i<- 1 to n)
+			numArray+=scala.io.StdIn.readInt()
+		println(numArray)
+		
+		val t=minmax(numArray)
+		println("Max is ",t._1)
+		println("Min is ",t._2)
+	}
+	def minmax(numArray: ArrayBuffer[Int]):(Int,Int)={
+		var min:Int=999
+		var max:Int=(-999)
+		for(value <- numArray){
+			if(value>max)
+				max = value
+			else if(value<min)
+				min = value
+		}
+		(max,min)
+	}
+} 
